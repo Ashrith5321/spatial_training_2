827cd8c23880d5784b428eac66529b27d7949af5
tccoin <junzhewu@umich.edu>
2026-01-18 05:39:51 -0500

    lighthouse compat

--- utils/vlm_worker.py
+++ utils/vlm_worker.py
@@ -900,7 +900,7 @@ class VLMTrainingMixin:
         torch.save(self.scheduler.state_dict(), os.path.join(path, "scheduler.pt"))
         print(f"✅ Checkpoint saved to: {path}")
 
-    def load_checkpoint(self, path, strict_base_check=True,load_optim=True,load_sched=True):
+    def load_checkpoint(self, path, strict_base_check=True,load_optim=True,load_sched=False):
         """
         Resumes training state fully. 
         Must be called AFTER setup_training().
@@ -936,7 +936,7 @@ class VLMTrainingMixin:
         sched_path = os.path.join(path, "scheduler.pt")
         if os.path.exists(sched_path) and load_sched:
             sched_state = torch.load(sched_path, map_location=self.accelerator.device)
-            self.scheduler.load_state_dict(sched_state)
+            self.scheduler.load_state_dict(sched_state,weights_only=True)
             print(" -> Scheduler loaded.")
 
 class DataGenerator:
